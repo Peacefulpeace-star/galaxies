07ce0dd6b22522aaaf69061050019e89ac2afe36
Peace <franklypeace@gmail.com>
2024-04-02 00:24:54 -0230

    Added redshift values

--- iras13224-3809.sh
+++ iras13224-3809.sh
@@ -2,4 +2,7 @@ Galaxy name = IRAS 13224-3809
 Galaxy black hole mass = 1.9e6
 Galaxy brightness variabiliy = High and random
 
-Galaxy redshift = 
+Galaxy redshift = ARK 120 0.03271
+GSN 069	0.018
+IRAS 13224-3809	0.066
+RE-J1034-396	0.0424 
